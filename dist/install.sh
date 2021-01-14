0d0da910aadb2bade380cfaee40288aa35f3efd3
pippistro <pippistro@gmail.com>
2021-01-14 19:44:12 +0000

    better installation

--- dist/install.sh
+++ dist/install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(echo $(cd ../ && pwd))"
 
 echo creating config file
-cp ../config/default.json.example-rtpengine ../config/local.json
+cp -f ../config/default.json.example-rtpengine ../config/local.json
 sed -i "#35\.195\.201\.218s#127\.0\.0\.1#g" ../config/local.json
 
 echo copying system file
-sudo cp -r etc/* /etc
-sudo cp -r lib/* /lib
+sudo cp -f -r etc/* /etc
+sudo cp -f -r lib/* /lib
 sudo sed -i "s#__HOMEPATH__#$DIR#g" /lib/systemd/system/siprec-drachtio.service
 
+echo npm install
+cd $DIR
+npm install
 
 echo restarting services
 sudo systemctl restart rsyslog
